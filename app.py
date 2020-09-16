70c972f50400a73435b35b697cd823718e48583b
Plu <kato.yugo@i.mbox.nagoya-u.ac.jp>
2020-09-16 12:43:57 +0900

    追加機能の作成

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os, sys, codecs, io
 from flask import Flask, request, render_template, Markup
 import shutil
 
+import db
+
 
 # 文字コードエラーへの対応
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
@@ -10,7 +12,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 app = Flask(__name__)
 
-
+db.make_db()
 
 @app.route('/index.html')
 def top_page():
@@ -32,6 +34,19 @@ def show_page():
 def main_page():
 
     return render_template('main.html', text="memo")
+
+@app.route('/main_add.html', methods=["POST"])
+def main1_page():
+    
+    title = request.form["title"]
+    link = request.form["link"]
+    description = request.form["description"]
+    
+    db.add_db(title, link, description)
+
+    result = db.show_db()
+
+    return render_template('main.html', result=Markup(result))
 
 
 if __name__ == "__main__":
